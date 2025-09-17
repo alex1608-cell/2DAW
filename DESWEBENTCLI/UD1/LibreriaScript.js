7b3287b539445a47f1411f67deb33a744436bdff
alex1608-cell <alejc2006@gmail.com>
2025-09-17 12:05:30 +0200

    primer commit

---
+++ DESWEBENTCLI/UD1/LibreriaScript.js
@@ -0,0 +1,12 @@
+function saludar(){
+    var edad = window.prompt('Introduce tu edad');
+    if(edad = 18){
+        console.log('Eres mayor de edad');
+    }else{
+        console.log('Eres menor de edad')
+    }
+   } 
+
+   function pintarRuta(){
+    console.log("El acceso a la ruta C:\\\\usuario\\tarda 1´23'', algo considerado \"lento\" en la actualidad")
+   }
