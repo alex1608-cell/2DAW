23679b20b3bd786a86adfdbc690a83cea4787989
alex1608-cell <alejc2006@gmail.com>
2025-10-21 13:46:14 +0200

    me moy a matar

--- DESWEBENTCLI/UD2 TypeScript/boletin1/script.js
+++ DESWEBENTCLI/UD2 TypeScript/boletin1/script.js
@@ -8,23 +8,34 @@ function horas() {
     console.log("La hora posterior es: " + (fecha.getHours() + 2));
 }
 // Ejercicio 2
+// [^\s@] -> Texto sin espacio
 function valida_form() {
-    console.log("Validando formulario...");
-    // // Otra forma de hacerlo
-    // // let expReg : RegExp = \lo_que_sea\;
-    var expReg = new RegExp("lo_que_sea");
-    // let email = document.getElementById("email") as HTMLInputElement | null;
-    // if (email && expReg.test(email.value)) {
-    // }
-    // Primero nos aseguramos que no es nulo ( porque no puedo almacenar un nulo en un HTMLInputElement)
-    if (document.getElementById("email") != null) {
-        var mensajeOk = document.createElement('span');
-        var email = document.getElementById("email");
-        if (email && expReg.test(email.value)) {
-            mensajeOk.textContent = "Email correcto";
-        }
-        else {
-            mensajeOk.textContent = "Email incorrecto";
-        }
+    var regExp = new RegExp("[^\s@]+@+[^\s@]+\.+[^\s@]+$"); // Con fallos ( hay que solucionar)
+    // Rescatamos las partes ( nodos ) necesarios.
+    var parrafoOk = document.getElementById("ok"); //Escritura
+    var parrafoError = document.getElementById("error"); //Escritura
+    //Aplico la validacion ( $inputValue me da el valor del "email") y miro con el $writeNode si es valido o no ( rudimentario )
+    if (regExp.test($inputValue("email"))) {
+        $writeNode("ok", "Email valido");
+        $writeNode("error", "");
+    }
+    else {
+        $writeNode("ok", "");
+        $writeNode("error", "Email no valido");
+    }
+}
+// Helpers ( comunes para todo el boletin.SIEMPRE EMPIEZAN POR DOLAR) (AQUI PILLAN EL VALOR DEL INPUT Y EL WRITE NODE ESCRIBE EL MENSAJE )
+function $inputValue(id) {
+    var input = document.getElementById(id); // Lectura0
+    var result = "";
+    if (input) {
+        result = input.value; // Aqui leo el valor
+    }
+    return result;
+}
+function $writeNode(id, msg) {
+    var nodo = document.getElementById(id); //Escritura
+    if (nodo) {
+        nodo.textContent = msg;
     }
 }
--- DESWEBENTCLI/UD2 TypeScript/boletin1/script.ts
+++ DESWEBENTCLI/UD2 TypeScript/boletin1/script.ts
@@ -15,31 +15,52 @@ function horas(): void {
 
 // Ejercicio 2
 
-function valida_form() {
-    console.log("Validando formulario...");
+// [^\s@] -> Texto sin espacio
 
-    // // Otra forma de hacerlo
-    // // let expReg : RegExp = \lo_que_sea\;
-    let expReg: RegExp = new RegExp("lo_que_sea");
+function valida_form(){
 
-    // let email = document.getElementById("email") as HTMLInputElement | null;
+    const regExp = new RegExp("[^\s@]+@+[^\s@]+\.+[^\s@]+$"); // Con fallos ( hay que solucionar)
 
-    // if (email && expReg.test(email.value)) {
+    // Rescatamos las partes ( nodos ) necesarios.
+    const parrafoOk = document.getElementById("ok") as HTMLElement; //Escritura
 
-    // }
+    const parrafoError = document.getElementById("error") as HTMLElement; //Escritura
 
 
-    // Primero nos aseguramos que no es nulo ( porque no puedo almacenar un nulo en un HTMLInputElement)
+    //Aplico la validacion ( $inputValue me da el valor del "email") y miro con el $writeNode si es valido o no ( rudimentario )
+    if(regExp.test($inputValue("email"))){
+        
+       $writeNode("ok", "Email valido");
+       $writeNode("error", "");
 
-    if(document.getElementById("email") != null){
-        let mensajeOk = document.createElement('span');
-
-        let email: HTMLInputElement = document.getElementById("email") as HTMLInputElement;
-
-        if(email && expReg.test(email.value)){
-            mensajeOk.textContent = "Email correcto"
-        }else{
-            mensajeOk.textContent = "Email incorrecto"
-        }
+    }else{
+        $writeNode("ok", "");
+        $writeNode("error", "Email no valido");
     }
 }
+
+
+// Helpers ( comunes para todo el boletin.SIEMPRE EMPIEZAN POR DOLAR) (AQUI PILLAN EL VALOR DEL INPUT Y EL WRITE NODE ESCRIBE EL MENSAJE )
+
+function $inputValue(id: string) : string{
+    
+    const input = document.getElementById(id) as HTMLInputElement; // Lectura0
+    let result = "";
+    
+    if(input){
+        result = input.value; // Aqui leo el valor
+    }
+    return result;
+}
+
+function $writeNode(id: string, msg: string) : void{
+  
+    const nodo = document.getElementById(id) as HTMLElement; //Escritura
+
+    if(nodo){
+        nodo.textContent = msg
+    }
+    
+}
+
+ 
